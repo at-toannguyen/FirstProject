2f6e772748c0b2577a0c1f57a14b3eb560fe95b8
Asiantech <asiantech@Tam-Tinh-Te.local>
2017-03-08 11:19:13 +0700

    add code

--- app/src/main/java/com/example/nhungnguyen/firstproject/MainActivity.java
+++ app/src/main/java/com/example/nhungnguyen/firstproject/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.nhungnguyen.firstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MyActivity";
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.spin);
+        //list item spinner
+        String arr[]={"asdasd","1231232","asdf23"};
+        //create adapter
+        ArrayAdapter<String> dataAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,arr);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int posision, long l) {
+        String item = adapterView.getItemAtPosition(posision).toString();
+        Toast.makeText(adapterView.getContext(), "U selected: "+ item, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
