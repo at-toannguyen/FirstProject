181cb82491eecd39a655fdad0b9e3c3f93652ad3
Asiantech <asiantech@Tam-Tinh-Te.local>
2017-03-09 16:45:46 +0700

    change code

--- app/src/main/java/com/example/nhungnguyen/firstproject/LoginActivity.java
+++ app/src/main/java/com/example/nhungnguyen/firstproject/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.nhungnguyen.firstproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mUser, mPass;
     private Button mBtnLg;
+    private TextView mTvCra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,18 @@ public class LoginActivity extends AppCompatActivity {
         mUser =(EditText) findViewById(R.id.edUser);
         mPass =(EditText)findViewById(R.id.edPass);
         mBtnLg =(Button)findViewById(R.id.btnLogin);
+        mTvCra=(TextView)findViewById(R.id.tvCra);
         mBtnLg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(LoginActivity.this, "User: "+ mUser.getText().toString()+" \n Pass: "+ mPass.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        mTvCra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
             }
         });
     }
---
+++ app/src/main/java/com/example/nhungnguyen/firstproject/RegisterActivity.java
@@ -0,0 +1,115 @@
+package com.example.nhungnguyen.firstproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity {
+    //Khai bao bien
+    private Button mBtn;
+    private EditText mUser, mPass;
+    private RadioButton mMale, mFemale;
+    private RadioGroup mRbgr;
+    private CheckBox mChk1, mChk2, mChk3, mChk4, mChk5, mChk6;
+    private ImageView mImgEye;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //khoi tao
+        mBtn = (Button) findViewById(R.id.chkRegister);
+        mUser = (EditText) findViewById(R.id.edUser);
+        mPass = (EditText) findViewById(R.id.edPass);
+        mRbgr = (RadioGroup) findViewById(R.id.rbgSex);
+        mMale = (RadioButton) findViewById(R.id.rbgMale);
+        mFemale = (RadioButton) findViewById(R.id.rbFemale);
+        mChk1 = (CheckBox) findViewById(R.id.cbkReadBook);
+        mChk2 = (CheckBox) findViewById(R.id.ckPlaySoccer);
+        mChk3 = (CheckBox) findViewById(R.id.chkListen_music);
+        mChk4 = (CheckBox) findViewById(R.id.chkPlayGame);
+        mChk5 = (CheckBox) findViewById(R.id.chkSwimming);
+        mChk6 = (CheckBox) findViewById(R.id.chkPlayBasketball);
+        mImgEye = (ImageView) findViewById(R.id.imgEye);
+        //set check cho cac radio button
+        int idchecked = mRbgr.getCheckedRadioButtonId();
+        switch (idchecked) {
+            case R.id.rbgMale:
+                break;
+            case R.id.rbFemale:
+                break;
+        }
+        // set onClick cho button
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this, "\n User: " + mUser.getText().toString() + " \n Pass: " + mPass.getText().toString() + " \n sex: " + setSex() + " \n Hobby: " + getTextCheckBox(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        //set hide/show password
+        mImgEye.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int x = Math.round(motionEvent.getX());
+                int y = Math.round(motionEvent.getY());
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mPass.setInputType(InputType.TYPE_CLASS_TEXT);
+                        Log.v("Test", "dơwn");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        mPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        Log.v("test", "up");
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //ham getText checkbox
+    public String getTextCheckBox() {
+        String text = "";
+        List<CheckBox> items = new ArrayList<CheckBox>();
+        items.add(mChk1);
+        items.add(mChk2);
+        items.add(mChk3);
+        items.add(mChk4);
+        items.add(mChk5);
+        items.add(mChk6);
+        for (CheckBox item : items) {
+            if (item.isChecked()) {
+                text += item.getText().toString() + " ";
+            }
+        }
+        return text;
+    }
+
+    //ham gettext radiobutton
+    public CharSequence setSex() {
+
+        if (mMale.isChecked()) {
+            return mMale.getText().toString();
+        } else {
+            if (mFemale.isChecked()) {
+                return mFemale.getText().toString();
+            } else {
+                return null;
+            }
+        }
+    }
+}
